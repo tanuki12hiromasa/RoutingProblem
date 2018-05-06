8128255241dde9e49e2ed86e095ae51ac57a71d7
Unknown <tanuki13.snow@gmail.com>
2018-05-07 01:50:51 +0900

    make algo03

---
+++ dijksta/path_algo03.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace dijksta
+{
+    class path_algo03:PathFinder
+    {
+        public path_algo03(int width, int height) : base(width, height) { }
+
+        protected override void makePath(Destination[] dest, int startpoint, out List<int> path)
+        {
+            base.makePath(dest, startpoint, out path);
+
+        }
+    }
+}
