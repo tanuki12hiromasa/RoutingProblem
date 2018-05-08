c65da395ab254168c290077574fea0fb79cb86c3
Unknown <tanuki13.snow@gmail.com>
2018-05-08 13:36:58 +0900

    update

--- dijksta/path_algo03.cs
+++ dijksta/path_algo03.cs
@@ -18,15 +18,24 @@ namespace dijksta
             bool update = true;
             while (update)
             {
-                for(int i = 0; i < _path.Count; i++)
+                for(int i = 1; i < _path.Count - 1; i++)
                 {
+                    var cDest = _path[i];
+                    for(int j = 0; j < _path.Count - 1; j++)
+                    {
+                        if((deltaCost(i-1,i)+deltaCost(i,i+1)+deltaCost(j,j+1)) - (deltaCost(i-1,i+1)+deltaCost(j,i)+deltaCost(i,j+1)) > 0)
+                        {
 
+                        }
+                    }
                 }
                 
                 update = false;
             }
 
             path = _path;
+
+            int deltaCost(int prev, int next) => dest[_path[prev]].cost[next];
         }
     }
 }
